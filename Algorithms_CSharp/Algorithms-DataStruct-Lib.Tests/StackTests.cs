76f621b9d756399afb8f6a397805abcb41ab2d3c
diegosmteixeira <diegosmteixeira@gmail.com>
2022-08-13 20:47:10 -0300

    StackTests - practicing Test Driven Development(TDD)

---
+++ Algorithms_CSharp/Algorithms-DataStruct-Lib.Tests/StackTests.cs
@@ -0,0 +1,58 @@
+﻿using NUnit.Framework;
+
+namespace Algorithms_DataStruct_Lib.Tests
+{
+    [TestFixture]
+    public class StackTests
+    {
+        [Test]
+        public void IsEmpty_EmptyStack_ReturnsTrue()
+        {
+            var stack = new ArrayStack<int>();
+            Assert.IsTrue(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Count_PushOneItem_ReturnsOne()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+
+            Assert.AreEqual(1, stack.Count);
+            Assert.IsFalse(stack.IsEmpty);
+        }
+
+        [Test]
+        public void Pop_EmptyStack_ThrowsException()
+        {
+            var stack = new ArrayStack<int>();
+
+            Assert.Throws<InvalidOperationException>(() => 
+            {
+                stack.Pop();
+            });
+        }
+
+        [Test]
+        public void Peek_PushTwoItems_ReturnHeadItem()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            Assert.AreEqual(2, stack.Peek());
+        }
+
+        [Test]
+        public void Peek_PushTwoItemsAndPop_ReturnsHeadElement()
+        {
+            var stack = new ArrayStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+
+            stack.Pop();
+
+            Assert.AreEqual(1, stack.Peek());
+        }
+    }
+}
